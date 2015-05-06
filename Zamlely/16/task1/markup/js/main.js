eaa9219fd7229de40e83540b29985bbc536f5010
nzamlely <n.zamlely@gmail.com>
2015-05-06 13:42:47 +0300

    commit homework 16 - task1

---
+++ Zamlely/16/task1/markup/js/main.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+	setHeight();
+	firstLiRedColor();
+	setHeightYellowBlock();
+	cleanBtnAction();
+	liIndexPrepend();
+	liActiveClass();
+
+	function setHeight(){
+		$('.container').each(function(index){
+			var boxes = $(this).find('.box');
+			if(!boxes.length){
+				return false;
+			}
+			//console.log('container ' + index)
+			//console.log("boxes.lenght = ", boxes.length)
+			//console.log(boxes)
+			var maxHeight = boxes.first().height();
+			//console.log('boxes', boxes)
+			//console.log('start maxHeight', maxHeight)
+			boxes.each(function(){
+				var currentHeight = $(this).height()
+				if(currentHeight > maxHeight){
+					maxHeight = currentHeight;
+				}
+			})
+			//console.log('maxHeight: ', maxHeight);
+			boxes.height(maxHeight);
+		})
+	}
+	
+	$(window).resize(setHeight);
+	
+	function setHeightYellowBlock() {
+		var blocks = $(".container").children(".block");
+		var maxHeight = blocks.first().height();
+		blocks.each(function(){
+			var currentHeight = $(this).height();
+				if(currentHeight > maxHeight) {
+					maxHeight = currentHeight;
+				}
+		});
+		blocks.height(maxHeight);
+	};
+		
+	function firstLiRedColor() {
+		var firstLi = $("li:first-child");
+		firstLi.css("color", "red");
+	};
+	
+	function cleanBtnAction(){
+		var cleanBtn = $(".btn-clean");
+		var formArea = $("input[type='text'], input[type='password']");
+		cleanBtn.click(function(){
+			formArea.val("");
+		});
+	};
+	
+	/*$(".btn-clean").click(function(){
+		$("input[type='text'], input[type='password']").val("")
+	}); */ 
+
+	function liIndexPrepend(){
+		$(".container").find("li:odd").each(function(){
+			var index = $(this).index();
+			$(this).prepend("№" + (index + 1) + " ");
+		});
+	};
+	
+	function liActiveClass(){
+		$("li").click(function(){
+			$(this).siblings(".active").removeClass();
+			$(this).addClass("active");
+		});
+	};
+	
+})
+	
