c9d2c19cd079cf24080c7cf6ae9148f8fac674c6
polovnik <alexandr.zanin@gmail.com>
2015-05-15 13:14:11 +0300

    corrected autoscaling menu

--- Zanin/Zanin_autoscaling-menu/js/main.js
+++ Zanin/Zanin_autoscaling-menu/js/main.js
@@ -3,17 +3,13 @@ $(function(){
 });
 
 function autoMenu(){
-	var outerWidth = $('#wrapper').outerWidth();//why do you need outerWidth of wrapper?
-	console.log('outerWidth is:', outerWidth);
 	var width = $('#wrapper').width();
-	console.log('width is:', width);
-	var padding = outerWidth - width;
-	console.log('padding is:', padding);
+	console.log('wrapper width:', width);
 	$('#nav li:last').each(function(){
-			var li = $(this).index()+1; //maybe clearer to use $('#nav li').length to get quantity of items?? and rename variable please: it's amount not li itself
-			console.log('li amount:',li);
+			var li = $('#nav li').length;
+			console.log('li:',li);
 
-			var liWidth = (outerWidth-(padding+(li*2))) / li; // width/li  - not the same? ))
+			var liWidth = (width-(li*2)) / li;
 			console.log('width of li:',liWidth);
 			$('#nav li a').css('width', liWidth);
 		})
