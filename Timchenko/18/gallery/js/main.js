e22145aa5e53ce949be3346dd93dd0fa888f815e
bogdan <bogdan@kredito.de>
2015-05-22 19:19:15 +0300

    gallery process

--- Timchenko/18/gallery/js/main.js
+++ Timchenko/18/gallery/js/main.js
@@ -1,3 +1,48 @@
 $(document).ready(function() {
 	gallerySlide();
 })
+
+
+function gallerySlide() {
+	var holder = $('#container');
+	var blockGallery = holder.find('.gallery-block');
+	var holderGallery = holder.find('.gallery-holder');
+	var control;
+	var itemsControl;
+	var listGallery;
+	var slides;
+	var currentGallery;
+	var slidesAmount;
+	var i = 0;
+
+
+	
+
+
+	blockGallery.each(function(index) {
+		currentGallery = $(this).addClass('gallery' + index);
+
+		currentGallery.append('<ul>');
+
+		listGallery = currentGallery.find('ul.gallery');
+		control = currentGallery.find('ul:not(.gallery)').addClass('control')
+		slides = listGallery.find('li');
+		slidesAmount = slides.length;
+
+
+		while (i < slidesAmount) {
+			control.append('<li>');
+			i++;
+		};
+
+		itemsControl = control.find('li');
+
+		console.log(itemsControl)
+
+		itemsControl.each(function(index){
+			$(this).append('<a>');
+			$(this).find('a').attr('href', '#').html(index)
+		});
+	});
+
+};
