61c2c8172395bc8fe3be92580c43b327d90c32b4
bogdan <bogdan@kredito.de>
2015-05-25 18:52:20 +0300

    changes

--- Timchenko/18/gallery/js/main.js
+++ Timchenko/18/gallery/js/main.js
@@ -6,20 +6,28 @@ function gallerySlide(galleryblock) {
 	var blockGallery = $(galleryblock);
 	var holderGallery = blockGallery.children('.gallery-holder');
 	var holderGalleryWidth = holderGallery.width();
-	var slidesWidth =holderGalleryWidth;
+	var slidesWidth = holderGalleryWidth;
 	var step = slidesWidth;
-	var control;
-	var itemsControl;
-	var linksControl;
-	var listGallery;
-	var slides;
-	var slidesAmount;
 	
-
 	blockGallery.each(function() {
 		var i = 0;
+		var control;
+		var itemsControl;
+		var linksControl;
+		var listGallery;
+		var slides;
+		var slidesAmount;
+		var btnPrev;
+		var btnNext;
+		var currentSlide;
+		var currentPosition;
+		var moveLeft;
+		var moveRight;
+		var flag = true;
 
 		$(this).append('<ul>');
+		btnPrev = $('<li>');
+		btnNext = $('<li>');
 
 		//gallery
 		listGallery = $(this).find('ul.gallery');
@@ -30,7 +38,6 @@ function gallerySlide(galleryblock) {
 		control = $(this).find('> ul').addClass('control');
 		//add li
 		while (i < slidesAmount) {
-			console.log(i)
 			control.append('<li>');
 			i++;
 		};
@@ -41,7 +48,62 @@ function gallerySlide(galleryblock) {
 			$(this).find('a').attr('href', '#').html(index + 1)
 		});
 
-		linksControl = control.find('a')
+		linksControl = control.find('a');
+
+		//prev next
+		btnPrev.append('<a>');
+		btnNext.append('<a>');
+		btnPrev.find('a').addClass('previous').text('PREV');
+		btnNext.find('a').addClass('next').text('NEXT');
+
+		control.prepend(btnPrev);
+		control.append(btnNext);
+
+		currentPosition = listGallery.css('marginLeft');
+		currentPosition = parseInt(currentPosition);
+
+		console.log(currentPosition)
+
+		moveLeft = function() {
+			currentPosition = listGallery.css('marginLeft');
+			currentPosition = parseInt(currentPosition);
+			listGallery.animate({
+				marginLeft: (currentPosition + step)
+			}, 200);
+			flag = true;
+		}
+		moveRight = function() {
+			currentPosition = listGallery.css('marginLeft');
+			currentPosition = parseInt(currentPosition);
+			listGallery.animate({
+				marginLeft: (currentPosition - step)
+			}, 200);
+			flag = false;
+		};
+
+		btnPrev.on('click', function(e) {
+			e.preventDefault();
+
+			if (flag === true ) {
+				moveLeft();
+			} else if (currentPosition > 0) {
+				return false;
+			};
+
+			flag = false;
+		});
+
+		/*btnNext.on('click', function(e) {
+			e.preventDefault();
+			flag = true;
+
+			if (flag === true ) {
+				moveRight();
+			} else if (currentPosition === (itemsControl * step)) {
+				return false;
+			};
+			console.log(currentPosition)	
+		});*/
 		
 		//show slide
 		linksControl.on('click', function(e) {
